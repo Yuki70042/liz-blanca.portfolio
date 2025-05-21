8d230f9deda9d89afe01e612f7d11fbe473c4813
Yuki70042 <lizlink2004@gmail.com>
2025-05-21 21:48:00 +0200

    js etudes debug

--- public/assets/js/Etudes.js
+++ public/assets/js/Etudes.js
@@ -1,19 +1,7 @@
 
-
-// Ajoute un menu "Burger sur les petits formats"
-document.getElementById('menu-toggle').onclick = function () {
-    var menu = document.getElementById('menu');
-    if (menu.classList.contains('active')) {
-        menu.classList.remove('active');
-    } else {
-        menu.classList.add('active');
-    }
-};
-
-
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("year").textContent = new Date().getFullYear();
-});
+})
 
 
 
